afbf79efc50a104f2bdf2d6e01d3b5b252efc937
sowjanya <sowji.muppala@gmail.com>
2025-11-06 21:49:42 +0530

    practice

---
+++ isDirOrFile.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+File_path=$1
+if [ -z "$File_path" ]; then
+    if [ -f "$File_path" ]; then
+        echo " it is a file"
+        if [ -r "$File_path" ]; then
+            echo "It is readable "
+        elif [ -w "$File_path" ]; then
+            echo "It is writable"
+        else 
+            echo "don't have permisssion to write and readable"
+        fi
+    if [ - "$File_path" ]; then
+        echo "It is a directory"
+        ls -l $File_path
+
+else
+    echo "file path is not exist"
+
+fi
