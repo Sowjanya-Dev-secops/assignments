020d1446a2af5dbd115a3da17a94bade8dbf1851
sowjanya <sowji.muppala@gmail.com>
2025-11-06 23:55:16 +0530

    practice

---
+++ 03-Top5RepetedWords.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+file_path=$1
+if [ -z "$file_path" ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+if [ ! -f "$file_path" ]; then
+    echo "Error:: file is not exist"
+    exit 1
+fi
+if [ ! -r "$file_path" ]; then
+    echo "file don't have rading permissions"
+    exit 1
+fi
+
+if [ ! -s "$file_path" ]; then
+    echo "file is empty"
+    exit 1
+fi
+
+tr '[:space:]' '[\n*]' < "$file_path" |
+tr -cd '[:alnum:]\n' |
+tr A-Z a-z|
+sort |
+uniq -c |
+sort nr |
+top -5
