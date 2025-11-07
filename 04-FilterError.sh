1280e34e34de35ddd90a57bf9a3a1210cb17722d
sowjanya <sowji.muppala@gmail.com>
2025-11-07 07:44:21 +0530

    practice

---
+++ 04-FilterError.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source_path=$1
+destination_path=$2
+if [ $# -lt 2 ]; then
+    echo "ERROR:: usage:$0 <source file > <Destination file> "
+    exit 1
+fi
+
+if [ ! -f $source_path ]; then
+    echo "source File doesnot exist "
+    exit 1
+fi
+
+while IFS = read r line
+do
+    if echo "$line" |grep -q 'ERROR';then
+        echo "$line" >> $destination_path
+    fi
+done <<< "$source_path"
