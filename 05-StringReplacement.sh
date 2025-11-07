0f97a3e5676013689c8cabcb2c5eb43cb95d550c
sowjanya <sowji.muppala@gmail.com>
2025-11-07 08:35:03 +0530

    practice

---
+++ 05-StringReplacement.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+file_path="$1"
+String1="FOO"
+String2="BAR"
+if [ ! -f "$file_path" ]; then
+    echo "ERROR:: file does not exist "
+    exit 1
+fi
+find $file_path -type f -name "*.conf" |
+while IFS= read -r line;
+do
+    sed -i 's/$String1/$String2/g' "$line"
+    echo "Updated file $filepath"
+done
