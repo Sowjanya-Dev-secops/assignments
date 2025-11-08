9bf5b13c12b423f93b1b6f50d125066b4ecbbc7d
sowjanya <sowji.muppala@gmail.com>
2025-11-08 12:01:58 +0530

    practice

--- 08-Fibonaci.sh
+++ 08-Fibonaci.sh
@@ -1,9 +1,9 @@
 Number_of_term=$1
-if [ -z "$Number_of_term"]; then
+if [ -z "$Number_of_term" ]; then
     echo "Usage:: $0 <please enter number of terms"
     exit 1
 fi
-if [ $Number_of_term -lt 0 ];
+if [ $Number_of_term -lt 0 ]; then
     echo "please enter the positive number"
     exit 1
 fi
