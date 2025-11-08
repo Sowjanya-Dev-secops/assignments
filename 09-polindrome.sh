146c4fabd09305aefab60752c7b884d93e75f190
sowjanya <sowji.muppala@gmail.com>
2025-11-08 12:53:25 +0530

    practice

---
+++ 09-polindrome.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+read -r "enter the string : " string
+if [ -z "$string" ]; then
+    echo "USAGE: $0 <please enter the string "
+    exit 1
+fi
+
+rev_string=$( echo "$string" | rev )
+if [ $string == $rev_string ]; then
+    echo " $string is polindrome"
+else    
+    echo " $string is not polindrome"
+
+fi
